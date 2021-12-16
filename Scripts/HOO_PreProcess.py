2d7dd6a2f80427c926f496e16bf2f66e75d39003
Callum <Callumr714@icloud.com>
2021-12-16 20:57:44 +0000

    HOO PreProcess Added

---
+++ Scripts/HOO_PreProcess.py
@@ -0,0 +1,44 @@
+
+'''
+R: integration_R (your file may not be called "integration" but whatever it is, the red channel will have the suffix "_R")
+G: (integration_B+integration_G)/2
+B: ((integration_B+integration_G)/2)*0.85 + integration_R*0.15
+https://pixinsight.com/forum/index.php?threads/beginner-need-help-in-processing-hoo.14228/
+
+This is the premise of this HOO preprocessor, made for people without a program with pixelmath functions.
+'''
+
+import sys
+import tifffile
+import numpy as np
+
+file = sys.argv[1]
+out = sys.argv[2]
+
+
+
+def HOO(file, out):
+    print("Loading Image")
+    img = tifffile.imread(file)
+    arr = np.array(img)
+
+    r = arr[:, :, 0]
+    g = arr[:, :, 1]
+    b = arr[:, :, 2]
+
+    print("Array converted")
+    print("Starting PixelMath")
+    arr[:, :, 0] = r
+    arr[:, :, 1] = (b + g) / 2
+    arr[:, :, 2] = ((b + g) / 2) * 0.85 + r * 0.15
+    print("Finished PixelMath")
+
+    tifffile.imsave(f"{out}.tiff", arr)
+    print("Finished writing to", out)
+
+if __name__ == "__main__":
+    HOO(file, out)
+
+
+
+
