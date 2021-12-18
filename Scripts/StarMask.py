0d9b10cb643206b251b238f7e91c04163a12d2f3
Callum <Callumr714@icloud.com>
2021-12-18 13:26:28 +0000

    Os type

---
+++ Scripts/StarMask.py
@@ -0,0 +1,15 @@
+import os
+import cv2 as cv
+import sys
+from sys import platform
+if platform == "linux" or platform == "linux2":
+    print("Linux not supported")
+elif platform == "darwin":
+    print("OS Type: Mac-os")
+    path_to = '../PreReq/Starnet++/OSX/StarNet_MacOS-2'
+elif platform == "win32":
+    print("OS Type: windows")
+    path_to = '../PreReq/Starnet++/Windows/StarNet_Win'
+
+image = sys.argv[1]
+output = sys.argv[2]
