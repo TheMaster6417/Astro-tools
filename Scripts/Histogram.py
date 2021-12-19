4574076e472191870c11661b4666915bcb546d99
GreenJon902 <GreenJon902@gmail.com>
2021-12-19 19:20:58 +0000

    fixed spelling in histogram

--- Scripts/Histogram.py
+++ Scripts/Histogram.py
@@ -5,14 +5,14 @@ import skimage.color
 file = "../TestImages/Downloaded Test image.tif"
 image = skimage.io.imread(file)
 
-transperancy = float(0.4)
+transparency = float(0.4)
 plt.xlabel('Intensity Value')
 plt.ylabel('Count')
 
 plt.hist(image.ravel(), bins = 128, color = 'grey', alpha = 0.5)
-plt.hist(image[:, :, 0].ravel(), bins = 128, color = 'red', alpha = transperancy)
-plt.hist(image[:, :, 1].ravel(), bins = 128, color = 'Green', alpha = transperancy)
-plt.hist(image[:, :, 2].ravel(), bins = 128, color = 'Blue', alpha = transperancy)
+plt.hist(image[:, :, 0].ravel(), bins = 128, color = 'red', alpha = transparency)
+plt.hist(image[:, :, 1].ravel(), bins = 128, color = 'Green', alpha = transparency)
+plt.hist(image[:, :, 2].ravel(), bins = 128, color = 'Blue', alpha = transparency)
 
 plt.legend(['Total', 'Red', 'Green', 'Blue'])
 try:
