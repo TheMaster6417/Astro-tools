aa0ad1cc7e484e97638570d63a76c6248632b510
GreenJon902 <GreenJon902@gmail.com>
2021-12-19 19:25:20 +0000

    reformated file

--- Scripts/ChromaticAberrationFixer.py
+++ Scripts/ChromaticAberrationFixer.py
@@ -12,6 +12,7 @@ image = sys.argv[1]
 image = cv2.imread(image)
 image = cv2.resize(image, (800, 800))
 
+
 def star_detection(image):  # based on https://github.com/TheMaster6417/StarRemoval/blob/main/starFunctions.py
     global imageG, mask, Detected
 
@@ -21,9 +22,7 @@ def star_detection(image):  # based on https://github.com/TheMaster6417/StarRemo
     template = cv2.imread('../Images/airplanesinthenightskyarelike.png', 0)
     w, h = template.shape[::-1]
 
-
     res = cv2.matchTemplate(imageG, template, cv2.TM_CCOEFF_NORMED)
-
 
     loc = np.where(res >= threshold)
     # masking by rotem on stackoverflow
@@ -31,21 +30,20 @@ def star_detection(image):  # based on https://github.com/TheMaster6417/StarRemo
 
     i = 0
 
-
-
     loc2 = list(zip(*loc[::-1]))
     pbar = tqdm.tqdm(len(loc2))
     for pt in loc2:
-        Detected = cv2.rectangle(image, pt, (pt[0] + w, pt[1] + h), (0,0,255), 1)
-        #cv2.rectangle(mask, (pt[0] + 4, pt[1] + 4), (pt[0] + w - 3, pt[1] + h - 3), 255, -1)
+        Detected = cv2.rectangle(image, pt, (pt[0] + w, pt[1] + h), (0, 0, 255), 1)
+        # cv2.rectangle(mask, (pt[0] + 4, pt[1] + 4), (pt[0] + w - 3, pt[1] + h - 3), 255, -1)
         # Reduce the size of the rectangle by 3 pixels from each side. old method by rotem
         cv2.circle(mask, (pt[0] + 4, pt[1] + 4), (w - h + 3), 255, -10)  # faster method
         pbar.update(1)
         # use both methods and it stil workss/might be better idk its late ok
-    #cv2.imshow("Detected",Detected)
+    # cv2.imshow("Detected",Detected)
     pbar.close()
     cv2.imshow("mask", mask)
     cv2.waitKey(0)
+
 
 def remove_dem(mask, image):
     mask = cv2.cvtColor(mask, cv2.COLOR_GRAY2BGR)
@@ -69,7 +67,8 @@ def remove_dem(mask, image):
     cv2.imshow("hsv", hsvImage)
     cv2.imshow("desaturated", hsvImageCopy)
 
-
     cv2.waitKey(0)
+
+
 star_detection(image)
 remove_dem(mask, image)
