c6e5014abf8351a64ca6acb1c6b1879eb5d5c784
Callum <Callumr714@icloud.com>
2022-02-01 21:33:30 +0000

    ThresholdSelection

---
+++ Scripts/SHO_PreProcess.py
@@ -0,0 +1,8 @@
+'''
+R = (Oiii^~Oiii)*Sii + ~(Oiii^~Oiii)*Ha
+G = ((Oiii*Ha)^~(Oiii*Ha))*Ha + ~((Oiii*Ha)^~(Oiii*Ha))*Oiii
+B = Oiii
+
+#https://thecoldestnights.com/2020/06/pixinsight-dynamic-narrowband-combinations-with-pixelmath/
+This is the premise of this SHO preprocessor, made for people without a program with pixelmath functions.
+'''
